b7c1cc576ce23b0e1b6d004c8d50b0f78da94595
Irene Escalona Calleja <iescalon@c3r7s2.42urduliz.com>
2024-03-12 15:15:49 +0100

    .h

--- src/so_long.h
+++ src/so_long.h
@@ -6,7 +6,16 @@
 /*   By: iescalon <iescalon@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 11:23:50 by iescalon          #+#    #+#             */
-/*   Updated: 2024/03/12 13:31:57 by iescalon         ###   ########.fr       */
+/*   Updated: 2024/03/12 13:54:30 by iescalon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SO_LONG_H
+#define SO_LONG_H
+
+#include "libft_gnl/libft.h"
+#include "ft_printf/ft_printf.h"
+#include "mlx/mlx.h"
+#include <stdbool.h>
+
+#endif
