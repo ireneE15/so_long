835e10dd8ce20a8182f3bd3817313ad99c6cb951
Irene Escalona Calleja <iescalon@c3r7s6.42urduliz.com>
2024-03-13 13:19:59 +0100

    correction of so_long.h

--- src/so_long.h
+++ src/so_long.h
@@ -6,22 +6,22 @@
 /*   By: iescalon <iescalon@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 11:23:50 by iescalon          #+#    #+#             */
-/*   Updated: 2024/03/13 12:49:44 by iescalon         ###   ########.fr       */
+/*   Updated: 2024/03/13 13:13:58 by iescalon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
-#define SO_LONG_H
+# define SO_LONG_H
 
-#include "libft_gnl/libft.h"
-#include "ft_printf/ft_printf.h"
-#include "mlx/mlx.h"
-#include <stdbool.h>
-#define W 'w'
-#define A 'a'
-#define S 's'
-#define D 'd'
-#define ESC 100
+# include "../libft_gnl/libft.h"
+# include "../ft_printf/ft_printf.h"
+# include "../mlx/mlx.h"
+# include <stdbool.h>
+# define W 'w'
+# define A 'a'
+# define S 's'
+# define D 'd'
+# define ESC 100
 
 typedef struct s_player
 {
