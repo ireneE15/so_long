e21b42be69470de486707c7e5c591df242db7ba4
Irene Escalona Calleja <iescalon@c3r7s6.42urduliz.com>
2024-03-13 13:53:16 +0100

    map funtion correction

--- src/map.c
+++ src/map.c
@@ -6,28 +6,20 @@
 /*   By: iescalon <iescalon@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 13:00:20 by iescalon          #+#    #+#             */
-/*   Updated: 2024/03/13 13:06:17 by iescalon         ###   ########.fr       */
+/*   Updated: 2024/03/13 13:26:10 by iescalon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int main() {
-    void *mlx_ptr;
-    void *win_ptr;
+int	main(void)
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
 
-    // Inicializar la conexión con el servidor gráfico
-    mlx_ptr = mlx_init();
-    if (mlx_ptr == NULL) {
-        return 1;
-    }
-
-    // Crear una ventana de 500x500 píxeles
-    win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Mi Ventana");
-
-    // Loop principal para mantener la ventana abierta
-    mlx_loop(mlx_ptr);
-
-    return 0;
+	mlx_ptr = mlx_init();
+	if (mlx_ptr == NULL)
+		return (1);
+	win_ptr = mlx_new_window(mlx_ptr, 1000, 1000, "So_long");
+	mlx_loop(mlx_ptr);
 }
-
